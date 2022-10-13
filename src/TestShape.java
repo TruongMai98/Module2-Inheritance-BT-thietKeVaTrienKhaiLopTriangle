e42398bd7ddd135b5a50f8b417dac9360c47083e
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-10-13 23:59:39 +0700

    thiet ke va trien khia lop triangle

---
+++ src/TestShape.java
@@ -0,0 +1,7 @@
+public class TestShape {
+    public static void main(String[] args) {
+        Shape sh1 = new Shape("yellow", true);
+
+        System.out.println(sh1.toString());
+    }
+}
---
+++ src/TestTriangle.java
@@ -0,0 +1,7 @@
+public class TestTriangle {
+    public static void main(String[] args) {
+        Triangle tri1 = new Triangle("red", true, 1.0, 1.0, 1.0);
+
+        System.out.println(tri1);
+    }
+}
